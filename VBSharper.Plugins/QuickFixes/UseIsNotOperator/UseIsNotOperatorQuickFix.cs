73428d324470317a52a9f46b28a559fffe5d0ba5
ThatShawGuy <ThatShawGuy@users.noreply.github.com>
2013-11-19 23:46:34 -0500

    Final Changes for v0.8.0

---
+++ VBSharper.Plugins/QuickFixes/UseIsNotOperator/UseIsNotOperatorQuickFix.cs
@@ -0,0 +1,29 @@
+using JetBrains.ReSharper.Feature.Services.Bulbs;
+using JetBrains.ReSharper.Psi.VB.Tree;
+using JetBrains.Util;
+using VBSharper.Plugins.QuickFixes.Base;
+
+namespace VBSharper.Plugins.QuickFixes.UseIsNotOperator
+{
+    [QuickFix]
+    public sealed class UseIsNotOperatorQuickFix : BulkQuickFixBase<ILogicalNotExpression>
+    {
+        public override string ProgressIndicatorTaskName { get { return "Applying IsNot Operator"; } }
+        public override string Text { get { return "Use IsNot operator"; } }
+        public override bool IsAvailable(IUserDataHolder cache) { return _highlighting.IsValid(); }
+
+        private readonly UseIsNotOperatorHighlighting _highlighting;
+
+        public UseIsNotOperatorQuickFix(UseIsNotOperatorHighlighting highlighting) 
+            : base(highlighting.Expression, new UseIsNotOperatorQuickFixHelper(), new UseIsNotOperatorFileQuickFix()) {
+            
+            _highlighting = highlighting;
+        }
+    }
+
+    public sealed class UseIsNotOperatorFileQuickFix : FileQuickFixBase<ILogicalNotExpression>
+    {
+        public override string Text { get { return "Use IsNot operator in file"; } }
+        protected override IQuickFixHelper<ILogicalNotExpression> QuickFixHelper { get { return new UseIsNotOperatorQuickFixHelper(); } }
+    }
+}
